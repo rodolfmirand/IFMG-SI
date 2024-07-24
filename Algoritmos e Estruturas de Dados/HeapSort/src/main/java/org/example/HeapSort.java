5f0c7d282cf42667e945dd748a8cc06bf392c57b
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-24 13:30:56 -0300

    AEDS - HeapSort

---
+++ Algoritmos e Estruturas de Dados/HeapSort/src/main/java/org/example/HeapSort.java
@@ -0,0 +1,60 @@
+package org.example;
+
+public class HeapSort {
+
+    public void sort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {38, 1, 4, 55, 8, 9, 0};
+
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+
+        System.out.println("Array ordenado é");
+        printArray(arr);
+    }
+    
+    static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
